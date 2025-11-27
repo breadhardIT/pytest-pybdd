28833bb677fa51d8c6087c6bd5d299320dbcad4b
Javier Ramos Panduro <javier.ramos@nextdigital.es>
2025-11-27 12:25:10 +0100

    b980

--- mutants/test/factory/documents_factory.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-from app.models.document import DocumentCreate
-
-
-def create_document_request_factory() -> DocumentCreate:
-    return DocumentCreate(
-        title=f"title_{uuid.uuid4().hex}", description=f"description {uuid.uuid4().hex}"
-    )
